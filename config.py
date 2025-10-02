de56d06dac6625e8352e71465091b7bafbb04d03
ShahByte <158125041+ShahByte@users.noreply.github.com>
2025-10-02 15:31:10 +0800

    config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ parser.add_argument('--midChannel', default=100, type=int)
 parser.add_argument('--outClust',  default=100, type=int)
 parser.add_argument('--maxIter', default=1000, type=int)
 parser.add_argument('--minLabels',  default=4, type=int)
-parser.add_argument('--lr',  default=0.01, type=float)
+parser.add_argument('--lr',  default=0.001, type=float)
 parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int)
 parser.add_argument('--model_dir', metavar='epoch dir', default='model_weights/',
                     help='File path', required=False)
