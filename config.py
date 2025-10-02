175b890a0da69fc101c0d8032cef6fd8b569897f
ShahByte <158125041+ShahByte@users.noreply.github.com>
2025-10-02 14:22:01 +0800

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description='PyTorch Unsupervised Segmentation'
 parser.add_argument('--inChannel', default=100,  type=int)
 parser.add_argument('--midChannel', default=100, type=int)
 parser.add_argument('--outClust',  default=100, type=int)
-parser.add_argument('--maxIter', default=300, type=int)
+parser.add_argument('--maxIter', default=1000, type=int)
 parser.add_argument('--minLabels',  default=4, type=int)
 parser.add_argument('--lr',  default=0.01, type=float)
 parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int)
